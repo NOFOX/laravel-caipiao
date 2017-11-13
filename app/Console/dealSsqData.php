c63651aa49c77cafe98310fd73ad6e3cff78d73f
wangzd <wangzhoudong@liweijia.com>
2017-11-13 18:09:46 +0800

    更新

--- app/Console/dealSsqData.php
+++ app/Console/dealSsqData.php
@@ -21,7 +21,7 @@ class dealSsqData
         $crawler->filter('.wqhgt tr')->each(function ($node,$i) {
             //只检查前两条是否有更新
             if($i>1 && $i<4) {
-                $data['issue'] = $node->filter('td ')->eq(1)->text();
+                $data['issue'] = $node->filter('td')->eq(1)->text();
                 $data['issue_date'] = $node->filter('td ')->eq(0)->text();
                 $data['red1'] = $node->filter('td ')->eq(2)->filter('em')->eq(0)->text();
                 $data['red2'] = $node->filter('td ')->eq(2)->filter('em')->eq(1)->text();
@@ -30,9 +30,9 @@ class dealSsqData
                 $data['red5'] = $node->filter('td ')->eq(2)->filter('em')->eq(4)->text();
                 $data['red6'] = $node->filter('td ')->eq(2)->filter('em')->eq(5)->text();
                 $data['blue'] = $node->filter('td ')->eq(2)->filter('em')->eq(6)->text();
-                $data['sales_amount'] = str_replace(',','',$node->filter('td ')->eq(3)->text());
-                $data['first_prize'] = $node->filter('td ')->eq(4)->filter('strong')->text();
-                $data['second_prize'] = $node->filter('td ')->eq(5)->filter('strong')->text();
+                $data['sales_amount'] = (int)trim(str_replace(',','',$node->filter('td')->eq(3)->filter('strong')->text()));
+                $data['first_prize'] = (int)$node->filter('td ')->eq(4)->filter('strong')->text();
+                $data['second_prize'] = (int)$node->filter('td ')->eq(5)->filter('strong')->text();
                 $ok = SsqModel::updateOrCreate(['issue'=>$data['issue']],$data);
                 if(!$ok) {
                     echo "创建失败" . $data['issue'] . "\r\n";
